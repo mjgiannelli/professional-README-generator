47252a52f0199906360057255e395c370e5319b3
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-06-28 10:20:50 -0500

    add pseudo code

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+// *** START PSEUDO CODE *** //
+
+// create a command line app that accepts user input
+//  * includes title of project, description, table of contents, installation, usage, license, contributing, tests, & questions
+// when user enters title, display it as the title of the README
+// When I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// user chooses a license for the app from a list of choices
+// * then a badge for that license is added near the top of the README and a notice is added to the section of the README entitled LICENSE that explains which license the app is covered for
+// when user enters GitHub username, then this is added to the section of the README entitled Questions with link to profile
+// when user enters email, then this is added to the section of the README entitled Questions with instructions on how to reach me with additional questions
+// When the user clicks the link in the Table of Contents, they are then taken to the corresponding section of the README
+
+// *** END PSEUDO CODE *** //
+
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
