7a59a8e1d86c120ac74522656ed0ceeb8a6fb45a
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-06-28 20:01:31 -0500

    add function to initialize app and store data

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const questions = [
     {
         type: 'input',
         name: 'title',
-        message: 'What is your title? (Required)',
+        message: 'What is the title of your project? (Required)',
         validate: titleInput => {
             if (titleInput) {
                 return true;
@@ -147,7 +147,11 @@ function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
 const init = () => {
-    return inquirer.prompt(questions);
+
+    return inquirer.prompt(questions)
+    .then(readmeData => {
+        console.log(readmeData);
+    })
 }
 
 // Function call to initialize app
